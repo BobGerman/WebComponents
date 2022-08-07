d6f9ee93a33a4faddf8b7ce2773d25339bccfed4
Bob German <bgerman1@hotmail.com>
2022-08-07 16:37:49 -0400

    Refactored

--- samples/stateful-button/index.js
+++ samples/stateful-button/index.js
@@ -2,29 +2,41 @@
     
     class statefulButton extends HTMLElement {
 
-        async connectedCallback () {
-            
-            const key = `state-${this.id}`;
-            let value = localStorage.getItem(key);
-            if (!value) {
-                this.innerHTML = "No value";
-            } else {
-                this.innerHTML = value;
-            }
-            this.onclick = () => {
-                if (value) {
-                    localStorage.removeItem(key);
-                    value = null;
-                    this.innerHTML = "No value";
-                } else {
-                    localStorage.setItem(key, "You clicked me");
-                    value = "You clicked me";
-                    this.innerHTML = "You clicked me";
-                }
-            }
+        #key;       // Local storage key for this button
+        #state;     // Current state for this button - falsy or true
 
+        constructor() {
+            super();
+            this.#key = `stateful-button-${this.id}`;
+            this.#state = localStorage.getItem(this.#key);
         }
-    }
+
+        #render() {
+            if (!this.#state) {
+                this.innerHTML = "OFF";
+            } else {
+                this.innerHTML = "ON";
+            }
+        }
+
+        #clickHandler() {
+            if (this.#state) {
+                this.#state = false;
+                localStorage.removeItem(this.#key);
+            } else {
+                this.#state = true;
+                localStorage.setItem(this.#key, this.#state);
+            }
+            this.#render();
+        }
+
+        async connectedCallback () {
+
+            this.onclick = this.#clickHandler;
+            this.#render();
+            
+        }
+}
 
     customElements.define('stateful-button', statefulButton);
     
