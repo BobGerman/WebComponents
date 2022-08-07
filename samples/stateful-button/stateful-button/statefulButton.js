4dfb4109f4acedcf13cf1ef898399673684a6e63
Bob German <bgerman1@hotmail.com>
2022-08-07 16:54:24 -0400

    Moved to ES module

---
+++ samples/stateful-button/stateful-button/statefulButton.js
@@ -0,0 +1,43 @@
+const BUTTON_KEY_PREFIX = 'stateful-button';
+
+export class statefulButton extends HTMLElement {
+
+    #key;       // Local storage key for this button
+    #state;     // Current state for this button - falsy or true
+
+    constructor() {
+        super();
+        this.#key = BUTTON_KEY_PREFIX + this.id;
+        this.#state = localStorage.getItem(this.#key);
+    }
+
+    #render() {
+        if (!this.#state) {
+            this.innerHTML = "OFF";
+        } else {
+            this.innerHTML = "ON";
+        }
+    }
+
+    #clickHandler() {
+        if (this.#state) {
+            this.#state = false;
+            localStorage.removeItem(this.#key);
+        } else {
+            this.#state = true;
+            localStorage.setItem(this.#key, this.#state);
+        }
+        this.#render();
+    }
+
+    async connectedCallback() {
+
+        this.onclick = this.#clickHandler;
+        this.#render();
+
+    }
+}
+
+customElements.define('stateful-button', statefulButton);
+
+
