61d012f7f53ebfd573d21eac223d6f32ae391188
Bob German <bgerman1@hotmail.com>
2022-08-07 16:19:41 -0400

    Simple toggle

--- samples/stateful-button/index.js
+++ samples/stateful-button/index.js
@@ -3,10 +3,26 @@
     class statefulButton extends HTMLElement {
 
         async connectedCallback () {
-            this.innerHTML = '<b>Hello, world</b>';
-            this.onclick = () => {
-                alert('You clicked me!');
+            
+            const key = `state-${this.id}`;
+            let value = localStorage.getItem(key);
+            if (!value) {
+                this.innerHTML = "No value";
+            } else {
+                this.innerHTML = value;
             }
+            this.onclick = () => {
+                if (value) {
+                    localStorage.removeItem(key);
+                    value = null;
+                    this.innerHTML = "No value";
+                } else {
+                    localStorage.setItem(key, "You clicked me");
+                    value = "You clicked me";
+                    this.innerHTML = "You clicked me";
+                }
+            }
+
         }
     }
 
