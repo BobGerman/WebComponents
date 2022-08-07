9b71a3f942ac1b9078870010f608e0dcfcf72619
Bob German <bgerman1@hotmail.com>
2022-08-07 16:07:15 -0400

    Rough out web component

--- samples/stateful-button/index.js
+++ samples/stateful-button/index.js
@@ -1,49 +1,16 @@
 (() => {
     
-    class Person {
-        
-        // Private fields
-        #firstName;
-        #lastName;
-        
-        constructor (firstName, lastName) {
-            this.#firstName = firstName;
-            this.#lastName = lastName;
-        }
-        
-        getName() {
-            return this.#firstName + " " + this.#lastName;
+    class statefulButton extends HTMLElement {
+
+        async connectedCallback () {
+            this.innerHTML = '<b>Hello, world</b>';
+            this.onclick = () => {
+                alert('You clicked me!');
+            }
         }
     }
 
-    class Geek extends Person {
-        
-        // Private fields
-        #favoriteDevLanguage;
-        #favoriteSciFiMovie;
-        
-        constructor (firstName, lastName, favoriteDevLanguage,
-                     favoriteSciFiMovie)
-                     {
-                         // The base class is called "super"
-                         super(firstName, lastName);
-                         this.#favoriteDevLanguage = favoriteDevLanguage;
-                         this.#favoriteSciFiMovie = favoriteSciFiMovie;
-                     }
-                     
-        getMessage() {
-            return super.getName() + " likes " + this.#favoriteDevLanguage + " and " + this.#favoriteSciFiMovie;
-        }
-                
-    }
-
-    var bob = new Geek ("Bob", "German", "C#", "The Matrix");
-    var julie = new Geek ("Julie", "Turner", "TypeScript", "Blade Runner");
-
-    document.getElementById('output').innerHTML = `
-    <p>${bob.getMessage()}</p>
-    <p>${julie.getMessage()}</p>
-    `;
+    customElements.define('stateful-button', statefulButton);
     
 })();
     
