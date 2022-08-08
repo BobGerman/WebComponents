4007324d3d5da525d44aff1b9258b42c2597afc6
Bob German <bgerman1@hotmail.com>
2022-08-08 12:14:28 -0400

    Now keeps buttons w/same ID's in sync

--- samples/stateful-button/stateful-button/statefulButton.js
+++ samples/stateful-button/stateful-button/statefulButton.js
@@ -1,26 +1,35 @@
 import stylesheet from './style.css' assert { type: 'css' };
 const BUTTON_KEY_PREFIX = 'stateful-button';
 
+// Singleton to store all buttons to keep them in sync
+const statefulButtons = {};
+
 export class statefulButton extends HTMLElement {
 
     #key;       // Local storage key for this button
-    #state;     // Current state for this button - falsy or true
     #button;
 
     constructor() {
         super();
         this.#key = BUTTON_KEY_PREFIX + this.id;
-        this.#state = localStorage.getItem(this.#key);
 
         const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.adoptedStyleSheets = [ stylesheet ];
         this.#button = document.createElement('button');
         shadowRoot.appendChild(this.#button);
-
     }
 
-    #render() {
-        if (!this.#state) {
+    #renderAll() {
+        if (statefulButtons[this.#key]) {
+            statefulButtons[this.#key].forEach((b) => {
+                b.render();
+            });
+        }
+    }
+
+    render() {
+        let state = localStorage.getItem(this.#key);
+        if (state) {
             this.#button.innerHTML = "OFF";
             this.#button.className = "buttonOff";
         } else {
@@ -30,20 +39,23 @@ export class statefulButton extends HTMLElement {
     }
 
     #clickHandler() {
-        if (this.#state) {
-            this.#state = false;
+        let state = localStorage.getItem(this.#key);
+        if (state) {
             localStorage.removeItem(this.#key);
         } else {
-            this.#state = true;
-            localStorage.setItem(this.#key, this.#state);
+            localStorage.setItem(this.#key, true);
         }
-        this.#render();
+        this.#renderAll();
     }
 
     async connectedCallback() {
 
+        if (!statefulButtons[this.#key]) {
+            statefulButtons[this.#key] = [];
+        }
+        statefulButtons[this.#key].push(this);
         this.onclick = this.#clickHandler;
-        this.#render();
+        this.render();
 
     }
 }
